70faf7137592e044273d07aeca6c4586e9b294b1
Rael <rael3014@gmail.com>
2024-12-03 19:05:04 -0300

    auth service

--- to_do_list/lib/services/auth_services.dart
+++ to_do_list/lib/services/auth_services.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthServices {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<User?> _signIn(String email, String password) async {
+  Future<User?> signIn(String email, String password) async {
     try {
       UserCredential result = await _auth.signInWithEmailAndPassword(
           email: email, password: password);
@@ -15,7 +15,7 @@ class AuthServices {
     }
   }
 
-  Future<User?> _register(String email, String password) async {
+  Future<User?> register(String email, String password) async {
     try {
       UserCredential result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
