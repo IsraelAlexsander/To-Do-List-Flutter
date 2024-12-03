ea57d52b6ed7daae265955d916ffc18a9540f4fe
Rael <rael3014@gmail.com>
2024-12-03 19:04:42 -0300

    tela de login e signup

--- to_do_list/lib/screens/login_screen.dart
+++ to_do_list/lib/screens/login_screen.dart
@@ -1,11 +1,111 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:to_do_list/screens/home_screen.dart';
+import 'package:to_do_list/screens/signup_screen.dart';
+import 'package:to_do_list/services/auth_services.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  final AuthServices _auth = AuthServices();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFF1d2630),
+      appBar: AppBar(
+        backgroundColor: Color(0xDD1d2630),
+        foregroundColor: Colors.white,
+        title: Text("Entrar"),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              SizedBox(height: 50),
+              Text(
+                "Entre aqui",
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: _emailController,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.white60),
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    labelText: "Email",
+                    labelStyle: TextStyle(
+                      color: Colors.white60,
+                    )),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: _passwordController,
+                style: TextStyle(color: Colors.white),
+                obscureText: true,
+                decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.white60),
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    labelText: "Senha",
+                    labelStyle: TextStyle(
+                      color: Colors.white60,
+                    )),
+              ),
+              SizedBox(height: 50),
+              SizedBox(
+                height: 50,
+                width: MediaQuery.of(context).size.width / 1.5,
+                child: ElevatedButton(
+                    onPressed: () async {
+                      User? user = await _auth.signIn(
+                          _emailController.text, _passwordController.text);
+                      if (user != null) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomeScreen()));
+                      }
+                    },
+                    child: Text(
+                      "Entrar",
+                      style: TextStyle(color: Colors.indigo, fontSize: 18),
+                    )),
+              ),
+              SizedBox(height: 20),
+              Text(
+                "OU",
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(height: 20),
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => SignupScreen()));
+                  },
+                  child: Text(
+                    "Criar conta",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- to_do_list/lib/screens/signup_screen.dart
+++ to_do_list/lib/screens/signup_screen.dart
@@ -1,11 +1,110 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:to_do_list/screens/home_screen.dart';
+import 'package:to_do_list/screens/login_screen.dart';
+import 'package:to_do_list/services/auth_services.dart';
 
 class SignupScreen extends StatelessWidget {
-  const SignupScreen({super.key});
+  final AuthServices _auth = AuthServices();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFF1d2630),
+      appBar: AppBar(
+        backgroundColor: Color(0xDD1d2630),
+        foregroundColor: Colors.white,
+        title: Text("Crie sua conta"),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              SizedBox(height: 50),
+              Text(
+                "Registre-se aqui",
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: _emailController,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.white60),
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    labelText: "Email",
+                    labelStyle: TextStyle(
+                      color: Colors.white60,
+                    )),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: _passwordController,
+                style: TextStyle(color: Colors.white),
+                obscureText: true,
+                decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.white60),
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    labelText: "Senha",
+                    labelStyle: TextStyle(
+                      color: Colors.white60,
+                    )),
+              ),
+              SizedBox(height: 50),
+              SizedBox(
+                height: 50,
+                width: MediaQuery.of(context).size.width / 1.5,
+                child: ElevatedButton(
+                    onPressed: () async {
+                      User? user = await _auth.register(
+                          _emailController.text, _passwordController.text);
+
+                      if (user != null) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomeScreen()));
+                      }
+                    },
+                    child: Text(
+                      "Registrar",
+                      style: TextStyle(color: Colors.indigo, fontSize: 18),
+                    )),
+              ),
+              SizedBox(height: 20),
+              Text(
+                "OU",
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(height: 20),
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()));
+                  },
+                  child: Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
