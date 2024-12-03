5a7178fb706be21d6f3c3d28ae151246128b4caf
Rael <rael3014@gmail.com>
2024-12-03 19:05:15 -0300

    inicio da home

--- to_do_list/lib/main.dart
+++ to_do_list/lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:to_do_list/screens/home_screen.dart';
 import 'package:to_do_list/screens/login_screen.dart';
 import 'package:to_do_list/screens/signup_screen.dart';
 
@@ -12,7 +14,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +23,7 @@ class MyApp extends StatelessWidget {
       title: "ToDo List",
       theme:
           ThemeData(primarySwatch: Colors.indigo, primaryColor: Colors.indigo),
-      home: SignupScreen(),
+      home: _auth.currentUser != null ? HomeScreen() : LoginScreen(),
     );
   }
 }
---
+++ to_do_list/lib/screens/home_screen.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:to_do_list/model/todo_model.dart';
+import 'package:to_do_list/screens/login_screen.dart';
+import 'package:to_do_list/services/auth_services.dart';
+import 'package:to_do_list/services/database.services.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _buttonIndex = 0;
+
+  final _widgets = [
+    Container(),
+    Container(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF1d2630),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF1d2630),
+        foregroundColor: Colors.white,
+        title: Text("ToDo List"),
+        actions: [
+          IconButton(
+              onPressed: () async {
+                await AuthServices().signOut();
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()));
+              },
+              icon: Icon(Icons.exit_to_app))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InkWell(
+                  borderRadius: BorderRadius.circular(10),
+                  onTap: () {
+                    setState(() {
+                      _buttonIndex = 0;
+                    });
+                  },
+                  child: Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width / 2.2,
+                    decoration: BoxDecoration(
+                      color: _buttonIndex == 0 ? Colors.indigo : Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Pendentes",
+                        style: TextStyle(
+                            fontSize: _buttonIndex == 0 ? 16 : 14,
+                            fontWeight: FontWeight.w500,
+                            color: _buttonIndex == 0
+                                ? Colors.white
+                                : Colors.black38),
+                      ),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  borderRadius: BorderRadius.circular(10),
+                  onTap: () {
+                    setState(() {
+                      _buttonIndex = 1;
+                    });
+                  },
+                  child: Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width / 2.2,
+                    decoration: BoxDecoration(
+                      color: _buttonIndex == 1 ? Colors.indigo : Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Completos",
+                        style: TextStyle(
+                            fontSize: _buttonIndex == 1 ? 16 : 14,
+                            fontWeight: FontWeight.w500,
+                            color: _buttonIndex == 1
+                                ? Colors.white
+                                : Colors.black38),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 30),
+            _widgets[_buttonIndex],
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
+        child: Icon(Icons.add),
+        onPressed: () {
+          _showTaskDialog(context);
+        },
+      ),
+    );
+  }
+
+  void _showTaskDialog(BuildContext context, {ToDo? todo}) {
+    final TextEditingController _titleController =
+        TextEditingController(text: todo?.title);
+    final TextEditingController _descriptionController =
+        TextEditingController(text: todo?.description);
+    final TextEditingController _finishIn = TextEditingController(
+        text: todo?.finish != null ? _formatDate(todo!.finish!) : '');
+    final DatabaseService _dbService = DatabaseService();
+
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            backgroundColor: Colors.white,
+            title: Text(todo == null ? "Criar Tarefa" : "Editar Tarefa"),
+          );
+        });
+  }
+
+  String _formatDate(DateTime date) {
+    return "${date.day.toString().padLeft(2, '0')}/"
+        "${date.month.toString().padLeft(2, '0')}/"
+        "${date.year}";
+  }
+}
